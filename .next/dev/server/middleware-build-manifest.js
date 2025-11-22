3d34b931ef7b3f2b36a2ce06095a1ca0cbe3ab9d
Burra Rajeev <burrarajeev22@gmail.com>
2025-11-22 19:21:13 +0530

    Fix: Add types for valid-url

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -11,17 +11,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/pages_index_2da965e7._.js",
       "static/chunks/turbopack-pages_index_1a9a3e1a._.js"
     ],
-    "/[code]": [
-      "static/chunks/node_modules_next_dist_compiled_8ca6b690._.js",
-      "static/chunks/node_modules_next_dist_shared_lib_82dc2e9d._.js",
-      "static/chunks/node_modules_next_dist_client_d0aa886c._.js",
-      "static/chunks/node_modules_next_dist_6024eba3._.js",
-      "static/chunks/node_modules_react-dom_4411d9bd._.js",
-      "static/chunks/node_modules_7f09fef0._.js",
-      "static/chunks/[root-of-the-server]__67f8542e._.js",
-      "static/chunks/pages_[code]_2da965e7._.js",
-      "static/chunks/turbopack-pages_[code]_a17a3d53._.js"
-    ],
     "/_app": [
       "static/chunks/node_modules_next_dist_compiled_8ca6b690._.js",
       "static/chunks/node_modules_next_dist_shared_lib_82dc2e9d._.js",
